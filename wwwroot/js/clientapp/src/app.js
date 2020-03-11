d54d6ca2c6ad2b936d3827d5c6b411d93b47753d
Sergey Lvov <sergey.lvov70@gmail.com>
2020-03-11 16:10:52 +0300

    init

---
+++ wwwroot/js/clientapp/src/app.js
@@ -0,0 +1,12 @@
+﻿import React, {Component} from 'react'
+import ReactDOM from 'react-dom'
+
+class Hello extends React.Component {
+    render() {
+        return <h1>Привет, React.JS</h1>;
+    }
+}
+ReactDOM.render(
+  <Hello />,
+  document.getElementById("content")
+);
---
+++ wwwroot/js/clientapp/webpack.config.js
@@ -0,0 +1,21 @@
+var path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/app.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'app.bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: "babel-loader"
+        }
+      },
+    ]
+}  
+};
